614eae5dafffb2dd6783c85ecfb2f83f84fb0536
nargizaladdinli <nargizalieva05@gmail.com>
2022-12-14 10:02:26 +0400

    Task test

---
+++ Home work/src/js/app.js
@@ -0,0 +1,18 @@
+//Pervaya rabota
+var $codlife = 12;
+var $coding = 14;
+var $seting = 'Hello'
+console.log($codlife+$coding+$seting);
+//Vtoraya rabota
+var $ad = "Nanuka";
+var $soyad = "Cavadze";
+var $ata = "Qoar";
+console.log($ad+$soyad+$ata);
+//let rabota odin
+let myAge = 16;
+let myName = 13;
+console.log(myAge-myName);
+//let rabota dva
+let sistem = 23;
+let style = 'sistem';
+console.log(sistem+style);
